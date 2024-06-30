b6f665ff2569d80cb6415b2bdb7650d70e54a8a4
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-29 18:44:27 -0700

    add TorchCorrelateValid

---
+++ src/sgnligo/transforms/correlate.py
@@ -0,0 +1,67 @@
+from . import *
+from ..base import *
+from typing import Any
+from torch.nn.functional import conv1d as Fconv1d
+
+import torch
+from sgn.base import TransformElement
+
+
+@dataclass
+class TorchCorrelateValid(TransformElement):
+    """
+    Correlates input data with filters
+
+    Parameters:
+    -----------
+    filters: Sequence[Any]
+        the filter to correlate over
+
+    Assumptions:
+    ------------
+    - There is only one sink pad and one source pad
+    """
+
+    filters: Sequence[Any] = None
+
+    def __post_init__(self):
+        assert self.filters is not None
+        self.shape = self.filters.shape
+        self.filters = self.filters.view(-1, 1, self.shape[-1])
+        super().__post_init__()
+        assert (
+            len(self.sink_pads) == 1 and len(self.source_pads) == 1
+        ), "only one sink_pad and one source_pad is allowed"
+
+    def pull(self, pad, frame):
+        self.frame = frame
+        assert frame.shape[-1] > self.shape[-1]
+
+    def corr(self, data):
+        return Fconv1d(data, self.filters, groups=data.shape[-2]).view(
+            self.shape[:-1] + (-1,)
+        )
+
+    def transform(self, pad):
+        """
+        Correlates data with filters
+        """
+        outbufs = []
+        frame = self.frame
+        for i, buf in enumerate(frame):
+            if buf.is_gap:
+                data = None
+            else:
+                # FIXME: Are there multi-channel correlation in numpy or scipy?
+                # FIXME: consider multi-dimensional filters
+                data = self.corr(buf.data)
+            outbufs.append(
+                SeriesBuffer(
+                    offset=buf.offset
+                    + Offset.fromsamples(self.shape[-1] - 1, buf.sample_rate),
+                    sample_rate=buf.sample_rate,
+                    data=data,
+                    shape=self.shape[:-1] + (buf.samples - self.shape[-1] + 1,),
+                )
+            )
+        return TSFrame(buffers=outbufs, EOS=frame.EOS)
