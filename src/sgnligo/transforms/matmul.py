e51f88a58dec50a5c30992659b052b771865d1eb
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-06-30 19:25:15 -0700

    update matmul

--- src/sgnligo/transforms/matmul.py
+++ src/sgnligo/transforms/matmul.py
@@ -27,47 +27,5 @@ class TorchMatmul(Matmul):
     - There is only one sink pad and one source pad
     """
 
-    matrix: Sequence[Any] = None
-
-    def __post_init__(self):
-        super().__post_init__()
-        assert (
-            len(self.sink_pads) == 1 and len(self.source_pads) == 1
-        ), "only one sink_pad and one source_pad is allowed"
-
-    def pull(self, pad, bufs):
-        """
-        Assumes there is only one sink pad, if the user wants
-        to matmul multitple channels of data,
-        connect multiple matmul elements
-        """
-        self.inbufs = bufs
-
     def matmul(self, a, b):
         return torch.matmul(a, b)
-
-    def transform(self, pad):
-        """
-        Matmul over list of buffers
-        """
-        inbufs = self.inbufs
-        outbufs = []
-        # loop over the input data, only perform matmul on non-gaps
-        EOS = inbufs.EOS
-        for inbuf in inbufs:
-            is_gap = inbuf.is_gap
-
-            if is_gap:
-                data = None
-            else:
-                data = self.matmul(self.matrix, inbuf.data)
-
-            outbuf = SeriesBuffer(
-                offset=inbuf.offset,
-                sample_rate=inbufs[-1].sample_rate,
-                data=data,
-                shape=self.matrix.shape[:-1] + (inbuf.samples,),
-            )
-            outbufs.append(outbuf)
-
-        return TSFrame(buffers=outbufs, EOS=EOS)
