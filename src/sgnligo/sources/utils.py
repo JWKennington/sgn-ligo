503214ea72fa66c5ccf0e19a384e102ee88c9b60
Rebecca Ewing <rebecca.ewing@ligo.org>
2024-07-02 11:19:13 -0700

    fix tabs, add T050017 parser

--- src/sgnligo/sources/utils.py
+++ src/sgnligo/sources/utils.py
@@ -1,11 +1,21 @@
 import lal
 from lal import LIGOTimeGPS
+
+import os
+
 import time
 
 def now():
-	"""
-	A convenience function to return the current gps time
-	"""
-	return LIGOTimeGPS(lal.UTCToGPS(time.gmtime()), 0)
+    """
+    A convenience function to return the current gps time
+    """
+    return LIGOTimeGPS(lal.UTCToGPS(time.gmtime()), 0)
 
+def from_T050017(url):
+    """
+    Parse a URL in the style of T050017-00.
+    """
+    filename, _ = os.path.splitext(url)
+    obs, desc, start, dur = filename.split("-")
+    return obs, desc, int(start), int(dur)
 
