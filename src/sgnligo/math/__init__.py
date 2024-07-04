604732b4d7dfef33e932ade90116c1d7c36c3c47
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-04 16:52:51 -0700

    math: add device and dtype

--- src/sgnligo/math/__init__.py
+++ src/sgnligo/math/__init__.py
@@ -2,20 +2,21 @@ import torch
 from torch.nn.functional import pad as tpad
 
 
-def cat_func(xs, axis):
-    return torch.cat(xs, dim=axis)
+class Math:
 
+    DEVICE = "cpu"
+    DTYPE = torch.float32
 
-def pad_func(data, pad_samples):
-    return tpad(data, pad_samples, "constant")
+    def cat_func(xs, axis):
+        return torch.cat(xs, dim=axis)
 
+    def pad_func(data, pad_samples):
+        return tpad(data, pad_samples, "constant")
 
-def full_func(shape, fill_value):
-    return torch.full(shape, fill_value)
+    def full_func(shape, fill_value):
+        return torch.full(shape, fill_value)
 
+    def stack_func(data, axis=0):
+        return torch.stack(data, axis)
 
-def stack_func(data, axis=0):
-    return torch.stack(data, axis)
-
-
-zeros_func = torch.zeros
+    zeros_func = lambda x: torch.zeros(x, device=Math.DEVICE, dtype=Math.DTYPE)
