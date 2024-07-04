2c5440c2591a72b707fefe7f8b6e30578b99c312
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-04 16:58:05 -0700

    converter.py: allow mulitple src and sinks

--- src/sgnligo/transforms/converter.py
+++ src/sgnligo/transforms/converter.py
@@ -17,6 +17,7 @@ class Converter(TSTransform):
     device: str = "cpu"
 
     def __post_init__(self):
+        assert set(self.source_pad_names) == set(self.sink_pad_names)
         super().__post_init__()
 
         if self.backend == "numpy":
@@ -36,8 +37,14 @@ class Converter(TSTransform):
         else:
             raise ValueError("Supported backends: 'numpy' or 'torch'")
 
+        self.pad_map = {
+            p: self.sink_pad_dict["%s:sink:%s" % (self.name, p.name.split(":")[-1])]
+            for p in self.source_pads
+        }
+
     def transform(self, pad):
-        frame = self.preparedframes[self.sink_pads[0]]
+        frame = self.preparedframes[self.pad_map[pad]]
+        self.preparedframes[self.pad_map[pad]] = None
 
         outbufs = []
         for buf in frame:
