9b2d0d82c3bb5aab0d3fc4df8fb9a83437aeca51
joshua.gonsalves <joshua.gonsalves@ligo.org>
2024-07-12 08:24:13 -0700

    Updated whitener and psd source pad.

--- src/sgnligo/transforms/__init__.py
+++ src/sgnligo/transforms/__init__.py
@@ -1,12 +1,19 @@
 from sgn.transforms import *
 from sgnts.transforms import *
-from sgnts.base.buffer import *
+from sgnts.base import (
+    SeriesBuffer,
+    TSFrame,
+    TSTransform,
+    AdapterConfig,
+)
+import os
+
 
 import numpy as np
 from gwpy.timeseries import TimeSeries
 
 @dataclass
-class Whiten(TransformElement):
+class Whiten(TSTransform):
     """
     Whiten input timeseries data
     Parameters:
@@ -17,61 +24,70 @@ class Whiten(TransformElement):
         sample rate of the data
     fft-length: int
         length of fft in seconds used for whitening
+    n: int
+        total number of samples in a single fft
+    z: int
+        zero padding length
+    nmed: int
+        how many previous samples we should account for when calcualting the geometric mean of the psd
+    navg: int
+        changes to the PSD must occur over a time scale of at least navg*(n/2 − z)*(1/sample_rate)
+        *check cody's paper for more info
+    ref_psd: str
+        file path to reference psd
+    psd_pad_name: str
+        pad name of the psd output source pad
     """
 
     whitening_method: str = "gwpy"
     sample_rate: int = 2048
     fft_length: int = 8
+    nmed: int = 7
+    navg: int = 64
+    n: int = fft_length * sample_rate
+    z: int = int(fft_length / 4. * sample_rate)
+
+    ref_psd: str = ""
+    psd_pad_name: str = ""
+
+    psd_buffer = []
+    psd_geometric_mean = []
+    psd_offset = None
+    prev_data = []
 
     def __post_init__(self):
+        if self.adapter_config is None:
+            self.adapter_config = AdapterConfig()
+        overlap = int(self.n/2 + self.z) 
+        self.adapter_config.overlap = (0, overlap) 
+        self.adapter_config.stride = self.n - overlap
+
         super().__post_init__()
+        
         self.inputs = {}
 
         # sanity check the whitening method given
         if self.whitening_method not in ("gwpy", "gstlal"):
             raise ValueError("Unknown whitening method, exiting.")
 
-        # set up a deque to store incoming data with audioadapter
-        self.audioadapters = {}
-
-        # track start offset of segment to whiten
-        self.seg_start = None
-
         # init the hann window
         self.window = self.hann_window()
 
-
-    def pull(self, pad, frame):
-        self.inputs[pad] = frame
-        # initialize audio adapter and push new data to deque
-        if pad not in self.audioadapters:
-            self.audioadapters[pad] = Audioadapter()
-        for buf in frame:
-            self.audioadapters[pad].push(buf)
-
-
-        # get offset segment
-        self.this_segment = (frame[0].offset, frame[-1].offset + frame[-1].noffset)
-        self.this_noffset = frame[-1].noffset + frame[-1].offset - frame[0].offset
-
-        # the first segment will start at the first buffer
-        if self.seg_start is None:
-            print(f"setting seg start to: {self.this_segment[0]}")
-            self.seg_start = self.this_segment[0]
-
-        print(f"{pad} this segment: {self.this_segment}")
-
+        
     def hann_window(self):
         """
         Apply hann window to input data
         """
         # number of samples in one fft-length segment
-        N = int(self.fft_length * self.sample_rate)
-        Z = int((self.fft_length / 4.) * self.sample_rate)
+        N = self.n
+        Z = self.z
         k = np.arange(0, N, 1) # indices
 
         hann = np.zeros(N)
         hann[Z:N-Z] = (np.sin(np.pi * (k[Z:N-Z] - Z)/(N - 2*Z)))**2
+
+        #FIXME gstlal had a method of adding from the two ends of the window so that small numbers weren't added to big ones
+        self.window_norm = np.sqrt(N / np.sum(hann ** 2))
         return hann
 
     def transform(self, pad):
@@ -79,43 +95,62 @@ class Whiten(TransformElement):
             Whiten incoming data in segments of fft-length seconds overlapped by fft-length / 4
             Some ascii art here would be helpful to illustrate what were doing.
             """
+            """
             metadata={
                 "name": "%s -> %s" % (
                     "+".join(f.metadata["name"] for f in self.inputs.values()),
                     pad.name,
                 )
             }
+            """
+            # incoming frame handling
+            outbufs = []
+            frame = self.preparedframes[self.sink_pads[0]]
             EOS=any(frame.EOS for frame in self.inputs.values()),
-        
-            frame = self.inputs[self.sink_pads[0]]
-            frame = frame[-1]
-            data = frame.data
+            outoffsets = self.preparedoutoffsets[self.sink_pads[0]]
 
-            # number of offsets in one segment to whiten
-            self.num_fft_offsets = int(self.fft_length * self.this_noffset)
+            # passes the psd along with an aligned attribute if the pad is the psd_pad
+            # if aligned == True, then the current offset == psd offset
+            if pad.name == self.psd_pad_name:
+                if self.psd_offset == None:
+                    return TSFrame(
+                            buffers = [SeriesBuffer(
+                                        offset=outoffsets[0]["offset"],
+                                        sample_rate=self.sample_rate,
+                                        data=None,
+                                        shape=frame.shape)],
+                            EOS = EOS,
+                            metadata = {"psd": None, "aligned": None})
+                else:
+                    aligned = (outoffsets[0]["offset"] == self.psd_offset)
+                    return TSFrame(
+                            buffers = [SeriesBuffer(
+                                        offset=outoffsets[0]["offset"],
+                                        sample_rate=self.sample_rate,
+                                        data=None,
+                                        shape=frame.shape)],
+                            EOS = EOS,
+                            metadata = {"psd": self.psd_geometric_mean, "aligned": aligned})
 
-            # check the length of data in the deque
-            offset_start, offset_end = self.audioadapters[self.sink_pads[0]].get_available_offset_segment()
-            data_length = offset_end - offset_start
-            if data_length > self.num_fft_offsets:
-                # flush one buffer of data from the left of the deque
-                offset_to_flush = offset_start + self.this_noffset
-                self.audioadapters[self.sink_pads[0]].flush_samples_by_end_offset_segment(offset_to_flush)
-            elif data_length < self.num_fft_offsets:
-                # we are  waiting for enough data at start up
-                return TSFrame(
-                                buffers = [SeriesBuffer(offset = frame.offset, noffset = 0, data = None, is_gap = True, offset_ref_t0 = frame.offset_ref_t0)],
-                                metadata = metadata,                                
-                                EOS = EOS)
+            # used for plotting purposes
+            offset_start = frame.buffers[0].offset
+            offset_end = frame.buffers[0].offset + frame.buffers[0].noffset
 
-            offset_start, offset_end = self.audioadapters[self.sink_pads[0]].get_available_offset_segment()
-
-            # check if it is time to compute the next inst. PSD
-            # ie have we shifted our data deque by fft-length / 4 since the last inst. PSD?
-            if offset_start >= self.seg_start:
+            # if audioadapter hasn't given us a frame, then waiting for more data
+            if frame.shape[-1] == 0:
+                outbufs.append(
+                    SeriesBuffer(
+                        offset=outoffsets[0]["offset"],
+                        sample_rate=self.sample_rate,
+                        data=None,
+                        shape=frame.shape,
+                    )
+                )
+            else:        
                 # copy samples from the deque
-                this_seg_data, _, _ = self.audioadapters[self.sink_pads[0]].copy_samples_by_offset_segment((offset_start, offset_end))
-
+                frame = frame.buffers[0]
+                this_seg_data = frame.data
+                
                 #np.savetxt(f"seg_data/seg_data_{offset_start}-{offset_end}.txt", this_seg_data)
 
                 if self.whitening_method == "gwpy":
@@ -130,47 +165,45 @@ class Whiten(TransformElement):
                     # transform back to a numpy array
                     whitened_data = np.array(whitened_data)
 
-                    #np.savetxt(f"seg_data/whitened_{offset_start}-{offset_end}.txt", whitened_data)
-
                 elif self.whitening_method == "gstlal":
                     # apply the window function - in gstlal we have used the Hann window
                     # but we could allow for different window functions here if we want to
-                    N = len(this_seg_data)
-                    #np.savetxt(f"seg_data/data_{offset_start}-{offset_end}.txt", this_seg_data)
-                    this_seg_data = self.window * this_seg_data
+
+                    # FIXME kinda goes for the entire gstlal whitener: some things here are different than gstlal - not good
+                    #       luckily it seems to be caused by some missing proportionality constant.
+                    this_seg_data = self.window * this_seg_data * self.window_norm #* 1/self.sample_rate
 
                     # apply fourier transform
                     freq_data = np.fft.rfft(this_seg_data)
-                    #np.savetxt(f"seg_data/ft_{offset_start}-{offset_end}.txt", freq_data)
                     # inst. PSD is proportional to the sq. magnitude
-                    psd_inst = (2/(N*(1/self.sample_rate))) * (np.abs(freq_data) ** 2)
-                    
-                    #np.savetxt(f"seg_data/psd_inst_{offset_start}-{offset_end}.txt", psd_inst)
 
-                    # compute median of PSDs, geometric mean, and arithmetic mean of PSDs
-                    if not hasattr(self, 'psd_buffer'):
-                        self.psd_buffer = []
-
-                    nmed = 7
-                    navg = 64
-
+                    # FIXME gstlal code multiply their psd by a 2*delta-f factor, but unsure where to do that
+                    psd_inst = 2 * self.sample_rate/self.n * (np.abs(freq_data) ** 2) 
+ 
+                    # compute median of PSDs, geometric mean, and arithmetic mean of PSDs 
+                    # keep track of last nmed psd_insts
                     self.psd_buffer.append(psd_inst)
-                    if len(self.psd_buffer) > nmed:
+                    if len(self.psd_buffer) > self.nmed:
                         self.psd_buffer.pop(0)
 
+                    # calculate median of psd_buffer
                     psd_median = np.median(self.psd_buffer, axis=0)
-                    #np.savetxt(f"seg_data/psd_med_{offset_start}-{offset_end}.txt", this_seg_data)
 
-                    # Calculate the geometric mean
-                    if not hasattr(self, 'psd_geometric_mean'):
+                    # create geometric mean template
+                    if self.psd_geometric_mean == []:
                         self.psd_geometric_mean = np.ones_like(psd_median)
 
+                    # load ref psd if necessary
+                    if self.ref_psd != "":
+                        self.psd_geometric_mean = np.loadtxt(self.ref_psd)
+                        self.ref_psd = ""
+
+                    # calculate geometric mean
+                    # FIXME this beta value appears in cody's paper, but idk what it is - and i havent found it in the gstlal code either 
                     beta = 1  # beta value
                     log_psd_median_adjusted = np.log(psd_median / beta)
-                    log_psd_geometric_mean = (navg - 1) / navg * np.log(self.psd_geometric_mean) + 1 / navg * log_psd_median_adjusted
+                    log_psd_geometric_mean = (self.navg - 1) / self.navg * np.log(self.psd_geometric_mean) + 1 / self.navg * log_psd_median_adjusted
                     self.psd_geometric_mean = np.exp(log_psd_geometric_mean)
-
-                    #np.savetxt(f"seg_data/psd_geo_{offset_start}-{offset_end}.txt", self.psd_geometric_mean)
 
                     # Estimate the arithmetic mean of the PSD
                     euler_gamma = np.exp(0.57721566490153286060)  # Euler-Mascheroni constant
@@ -179,25 +212,38 @@ class Whiten(TransformElement):
                     # whiten by dividing data by the square root of the PSD
                     # in each frequency bin
                     freq_data_whitened = freq_data / np.sqrt(psd_arithmetic_mean)
-                    
-                    #np.savetxt(f"seg_data/freq_whitened_{offset_start}-{offset_end}.txt", freq_data_whitened)
 
                     # Fourier Transform back to the time domain
-                    whitened_data = np.fft.irfft(freq_data_whitened, N)
-
+                    whitened_data = np.fft.irfft(freq_data_whitened, self.n)
                     #np.savetxt(f"seg_data/whitened_{offset_start}-{offset_end}.txt", whitened_data)
-                # shift the next segment start by the overlap
-                self.seg_start += int(self.num_fft_offsets / 4)
-                print(f"setting seg start to: {self.seg_start}")
+                    
+                    whitened_data *= (1/self.sample_rate) * np.sqrt(np.sum(self.window ** 2))
 
-                return TSFrame(
-                                buffers = [SeriesBuffer(offset = offset_start, noffset = offset_end - offset_start, data = whitened_data, offset_ref_t0 = frame.offset_ref_t0)],
-                                metadata = metadata,
-                                EOS = EOS)
+                    # accounts for overlap by summing with prev_data over the stride of the adapter
+                    if self.prev_data == []:
+                        self.prev_data = whitened_data[self.adapter_config.stride:]
+                    else:
+                        whitened_data[:self.adapter_config.overlap[1]] += self.prev_data
+                        self.prev_data = whitened_data[self.adapter_config.stride:]
+                    
+                    """
+                    if EOS:
+                        np.savetxt(f"seg_data/psd_geo.txt", self.psd_geometric_mean)
+                    """
+                    # offset of most recent psd
+                    self.psd_offset = outoffsets[0]["offset"]
 
-            else:
-                # its not time for the next segment yet
-                return TSFrame(
-                                buffers = [SeriesBuffer(offset = frame.offset, noffset = 0, data = None, is_gap=True, offset_ref_t0 = frame.offset_ref_t0)],
-                                metadata = metadata,
-                                EOS = EOS)
+                # only output data up till the length of the adapter stride      
+                outbufs.append(
+                    SeriesBuffer(
+                        offset=outoffsets[0]["offset"],
+                        sample_rate=self.sample_rate,
+                        data=whitened_data[:self.adapter_config.stride],
+                    )
+                )
+
+            # return frame with the correct buffers
+            return TSFrame(
+                            buffers = outbufs,
+                            #metadata = metadata,
+                            EOS = EOS)
--- test/test_whiten.py
+++ test/test_whiten.py
@@ -4,6 +4,9 @@ from sgn.apps import Pipeline
 from sgnts.sources import FakeSeriesSrc
 from sgnts.sinks import DumpSeriesSink
 from sgnligo.transforms import Whiten
+from sgnligo.sources import FrameReader
+from sgnts.transforms import Resampler
+import os
 
 def test_whitengraph(capsys): 
 
@@ -25,25 +28,42 @@ def test_whitengraph(capsys):
     #           ------ 
     #
 
+    output_dir = '/home/joshua.gonsalves/sgn-ligo/seg_data'
+    os.makedirs(output_dir, exist_ok=True)
     
-    
-    pipeline.insert(FakeSeriesSrc(
+    pipeline.insert(FrameReader(
                name = "src1",
                source_pad_names = ("H1",),
-               num_buffers = 32,
-               duration = 1,
-               signal_type = 'white',
+               rate=16384,
+               num_samples=16384,
+               framecache="test/gw190425.cache",
+               channel_name = ("GWOSC-16KHZ_R1_STRAIN",),
+               instruments = ("L1",),
+             ),
+             Resampler(
+               name="trans1",
+               source_pad_names=("H1",),
+               sink_pad_names=("H1",),
+               inrate=16384,
+               outrate=2048,
              ),
              Whiten(
                name = "whiten",
-               source_pad_names = ("H1",),
+               source_pad_names = ("H1","H2"),
                sink_pad_names = ("H1",),
-               whitening_method = "gwpy",
+               whitening_method = "gstlal",
+               ref_psd = "/home/joshua.gonsalves/sgn-ligo/psd_geo.txt",
+               psd_pad_name = "whiten:src:H2"
              ),
              DumpSeriesSink(
                name = "snk1",
                sink_pad_names = ("H1",),
                fname = 'out.txt'
+             ),
+             DumpSeriesSink(
+                 name = "snk3",
+                 sink_pad_names = ("H2",),
+                 fname = 'psd_out.txt'
              )
     )
 
@@ -53,9 +73,11 @@ def test_whitengraph(capsys):
            fname = 'in.txt'
          ))
     pipeline.insert(link_map={
-                              "whiten:sink:H1":"src1:src:H1",
+                              "trans1:sink:H1": "src1:src:H1",
+                              "whiten:sink:H1":"trans1:src:H1",
                               "snk1:sink:H1":"whiten:src:H1",
-                              "snk2:sink:H1":"src1:src:H1"
+                              "snk2:sink:H1":"src1:src:H1",
+                              "snk3:sink:H2":"whiten:src:H2"
                               })
 
     pipeline.run()
