3c69a22d7fbbcafe66cac69ca240f5c9996aeeb6
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-21 14:34:57 -0700

    greg_inspiral.py: black

--- src/sgnligo/bin/greg_inspiral.py
+++ src/sgnligo/bin/greg_inspiral.py
@@ -21,6 +21,7 @@ from sgnligo.transforms import (
     Itacacac,
 )
 
+
 def parse_command_line():
     parser = OptionParser()
 
@@ -37,16 +38,60 @@ def parse_command_line():
         default=10,
         help="Number of buffers the source element should produce when source is fake source",
     )
-    parser.add_option("--gps-start-time", metavar = "seconds", help="Set the start time of the segment to analyze in GPS seconds.")
-    parser.add_option("--gps-end-time", metavar = "seconds", help="Set the end time of the segment to analyze in GPS seconds.")
-    parser.add_option("--sample-rate", metavar = "Hz", type = int, default=16384, help="Requested sampling rate of the data.")
-    parser.add_option("--frame-cache", metavar = "file", help="Set the path to the frame cache file to analyze.")
-    parser.add_option("--channel-name", metavar = "channel", help = "Name of the data channel to analyze.")
-    parser.add_option("--whitening-method", metavar = "algorithm", default = "gstlal", help = "Algorithm to use for whitening the data. Supported options are 'gwpy' or 'gstlal'. Default is gstlal.")
-    parser.add_option("--reference-psd", metavar = "file", help = "load the spectrum from this LIGO light-weight XML file (optional).")
-    parser.add_option("--track-psd", action = "store_true", help = "Enable dynamic PSD tracking.  Always enabled if --reference-psd is not given.")
-    parser.add_option("--psd-fft-length", action = "store", type=int, help = "The fft length for psd estimation.")
-    parser.add_option("--ht-gate-threshold", action = "store", type=float, default=float('+inf'), help = "The gating threshold. Data above this value will be gated out.")
+    parser.add_option(
+        "--gps-start-time",
+        metavar="seconds",
+        help="Set the start time of the segment to analyze in GPS seconds.",
+    )
+    parser.add_option(
+        "--gps-end-time",
+        metavar="seconds",
+        help="Set the end time of the segment to analyze in GPS seconds.",
+    )
+    parser.add_option(
+        "--sample-rate",
+        metavar="Hz",
+        type=int,
+        default=16384,
+        help="Requested sampling rate of the data.",
+    )
+    parser.add_option(
+        "--frame-cache",
+        metavar="file",
+        help="Set the path to the frame cache file to analyze.",
+    )
+    parser.add_option(
+        "--channel-name", metavar="channel", help="Name of the data channel to analyze."
+    )
+    parser.add_option(
+        "--whitening-method",
+        metavar="algorithm",
+        default="gstlal",
+        help="Algorithm to use for whitening the data. Supported options are 'gwpy' or 'gstlal'. Default is gstlal.",
+    )
+    parser.add_option(
+        "--reference-psd",
+        metavar="file",
+        help="load the spectrum from this LIGO light-weight XML file (optional).",
+    )
+    parser.add_option(
+        "--track-psd",
+        action="store_true",
+        help="Enable dynamic PSD tracking.  Always enabled if --reference-psd is not given.",
+    )
+    parser.add_option(
+        "--psd-fft-length",
+        action="store",
+        type=int,
+        help="The fft length for psd estimation.",
+    )
+    parser.add_option(
+        "--ht-gate-threshold",
+        action="store",
+        type=float,
+        default=float("+inf"),
+        help="The gating threshold. Data above this value will be gated out.",
+    )
     parser.add_option(
         "--svd-bank",
         metavar="filename",
@@ -127,32 +172,9 @@ def parse_command_line():
     return options, args
 
 
-# Read in the svd banks!
-#svd_bank = [
-#    #"H1-0750_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    #"L1-0750_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    #"V1-0750_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    "H1-0250_GSTLAL_SVD_BANK_half-0-0.xml.gz",
-#    "L1-0250_GSTLAL_SVD_BANK_half-0-0.xml.gz",
-#    "V1-0250_GSTLAL_SVD_BANK_half-0-0.xml.gz",
-#    # "/home/yun-jing.huang/phd/o3mdc-filters/mdc11/svd_bank/H1-0000_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    # "/home/yun-jing.huang/phd/o3mdc-filters/mdc11/svd_bank/H1-0101_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    # "/home/yun-jing.huang/phd/o3mdc-filters/mdc11/svd_bank/H1-0102_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    # "/home/yun-jing.huang/phd/o3mdc-filters/mdc11/svd_bank/L1-0000_GSTLAL_SVD_BANK-0-0.xml.gz",
-#    # "/home/yun-jing.huang/phd/o3mdc-filters/mdc11/svd_bank/V1-0000_GSTLAL_SVD_BANK-0-0.xml.gz",
-#]
-#impulse_bankno = 0
-#original_templates = "full_templates_bin0250_tol999_1024.hdf5"
-#nbank_pretend = 0
-#nslice = -1
-#verbose = True
-#device = "cpu"
-#dtype = torch.float32
-
 def main():
     # parse arguments
     options, args = parse_command_line()
-
 
     dtype = options.torch_dtype
     if dtype == "float64":
@@ -164,9 +186,6 @@ def main():
     else:
         raise ValueError("Unknown data type")
 
-    copy_block = 1
-    #impulse = True
-
     if options.impulse_bank is not None:
         impulse = True
     else:
@@ -177,13 +196,15 @@ def main():
 
     # read in the svd banks
     banks = group_and_read_banks(
-        svd_bank=options.svd_bank, nbank_pretend=options.nbank_pretend, nslice=options.nslice, verbose=True
+        svd_bank=options.svd_bank,
+        nbank_pretend=options.nbank_pretend,
+        nslice=options.nslice,
+        verbose=True,
     )
 
     # sort and group the svd banks by sample rate
     sorted_bank = SortedBank(
         banks=banks,
-        copy_block=copy_block,
         device=options.torch_device,
         dtype=dtype,
         nbank_pretend=options.nbank_pretend,
@@ -203,50 +224,54 @@ def main():
         if options.data_source == "frames":
             pipeline.insert(
                 FrameReader(
-                   name = ifo+"_FrameReader",
-                   source_pad_names = (ifo,),
-                   rate=options.sample_rate,
-                   num_samples=options.num_samples,
-                   framecache=options.frame_cache,
-                   channel_name = options.channel_name,
-                   instrument = ifo, 
-                   gps_start_time = options.gps_start_time,
-                   gps_end_time = options.gps_end_time,
+                    name=ifo + "_FrameReader",
+                    source_pad_names=(ifo,),
+                    rate=options.sample_rate,
+                    num_samples=options.num_samples,
+                    framecache=options.frame_cache,
+                    channel_name=options.channel_name,
+                    instrument=ifo,
+                    gps_start_time=options.gps_start_time,
+                    gps_end_time=options.gps_end_time,
                 ),
-                 Resampler(
-                   name=ifo+"_FrameResampler",
-                   sink_pad_names=(ifo,),
-                   source_pad_names=(ifo,),
-                   inrate=options.sample_rate,
-                   outrate=maxrate,
-                 ),
-                 Whiten(
-                   name = ifo+"_Whitener",
-                   sink_pad_names = (ifo,),
-                   source_pad_names = (ifo,),
-                   instrument = ifo,
-                   sample_rate = maxrate,
-                   fft_length = options.psd_fft_length,
-                   whitening_method = options.whitening_method,
-                   reference_psd = options.reference_psd,
-                   #psd_pad_name = "Whitener:src:spectrum"
-                 ),
-                 Threshold(
-                    name=ifo+"_Threshold",
+                Resampler(
+                    name=ifo + "_FrameResampler",
+                    sink_pad_names=(ifo,),
+                    source_pad_names=(ifo,),
+                    inrate=options.sample_rate,
+                    outrate=maxrate,
+                ),
+                Whiten(
+                    name=ifo + "_Whitener",
+                    sink_pad_names=(ifo,),
+                    source_pad_names=(ifo,),
+                    instrument=ifo,
+                    sample_rate=maxrate,
+                    fft_length=options.psd_fft_length,
+                    whitening_method=options.whitening_method,
+                    reference_psd=options.reference_psd,
+                    # psd_pad_name = "Whitener:src:spectrum"
+                ),
+                Threshold(
+                    name=ifo + "_Threshold",
                     source_pad_names=(ifo,),
                     sink_pad_names=(ifo,),
-                    threshold = options.ht_gate_threshold,
-                    startwn = maxrate//2,
-                    stopwn = maxrate//2,
-                    invert = True,
-                 ),
+                    threshold=options.ht_gate_threshold,
+                    startwn=maxrate // 2,
+                    stopwn=maxrate // 2,
+                    invert=True,
+                ),
             )
             pipeline.insert(
-                    link_map = {
-                              ifo+"_FrameResampler:sink:"+ifo: ifo+"_FrameReader:src:"+ifo,
-                              ifo+"_Whitener:sink:"+ifo: ifo+"_FrameResampler:src:"+ifo,
-                              ifo+"_Threshold:sink:"+ifo: ifo+"_Whitener:src:"+ifo,
-                              }
+                link_map={
+                    ifo
+                    + "_FrameResampler:sink:"
+                    + ifo: ifo
+                    + "_FrameReader:src:"
+                    + ifo,
+                    ifo + "_Whitener:sink:" + ifo: ifo + "_FrameResampler:src:" + ifo,
+                    ifo + "_Threshold:sink:" + ifo: ifo + "_Whitener:src:" + ifo,
+                }
             )
             lloid_input_source_link[ifo] = ifo + "_Threshold:src:" + ifo
         else:
@@ -273,7 +298,15 @@ def main():
             lloid_input_source_link[ifo] = ifo + "_src:src:" + ifo
 
     # connect LLOID
-    lloid_output_source_link = lloid(pipeline, sorted_bank, lloid_input_source_link, options.num_samples, options.nslice, options.torch_device, dtype)
+    lloid_output_source_link = lloid(
+        pipeline,
+        sorted_bank,
+        lloid_input_source_link,
+        options.num_samples,
+        options.nslice,
+        options.torch_device,
+        dtype,
+    )
 
     # make the sink
     impulse_ifo = "H1"
@@ -318,14 +351,13 @@ def main():
             link_map={"StrikeSnk:sink:trigs": "itacacac:src:trigs"},
         )
 
-
     # link output of lloid
     for ifo, link in lloid_output_source_link.items():
         if impulse:
             pipeline.insert(
                 link_map={
                     "imsink0:sink:" + ifo: link,
-               }
+                }
             )
             if ifo == impulse_ifo:
                 pipeline.insert(
@@ -343,6 +375,7 @@ def main():
 
     # Run pipeline
     pipeline.run()
+
 
 if __name__ == "__main__":
     main()
