595718bed90179509e96466c1d3397f2366f1e87
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-24 20:41:54 -0400

    update kafka_sink

--- src/sgnligo/sinks/kafka_sink.py
+++ src/sgnligo/sinks/kafka_sink.py
@@ -20,8 +20,6 @@ class KafkaSink(TSSink):
         The kafka topic to write data to
     metadata_key: str
         The dictionary key of the frame metadata to retrieve the data
-    route:
-        The route of the kafka data
     tags: str
         The tags to write the kafka data
     verbose: bool
@@ -32,7 +30,8 @@ class KafkaSink(TSSink):
     output_kafka_server: str = None
     topic: str = None
     metadata_key: str = None
-    route: str = None
+    prepare_kafka_data: bool = False
+    routes: str = None
     tags: list = None
     verbose: bool = False
     reduce_time: float = 2
@@ -47,7 +46,7 @@ class KafkaSink(TSSink):
         self.last_reduce_time = None
 
         self.client = kafka.Client("kafka://{}".format(self.output_kafka_server))
-        self.kafka_data = defaultdict(lambda: {'time': [], 'data': []})
+        #self.kafka_data = defaultdict(lambda: {'time': [], 'data': []})
         self.last_t0 = None
 
     def pull(self, pad, bufs):
@@ -57,21 +56,31 @@ class KafkaSink(TSSink):
         """
         self.cnt[pad] += 1
         bufst0 = bufs[0].t0/1_000_000_000
+        metadata = bufs.metadata
         if self.last_t0 is None:
             self.last_t0 = bufst0
 
-        # append data to deque
-        if self.metadata_key in bufs.metadata:
-            self.kafka_data[self.route]['time'].append(bufst0)
-            self.kafka_data[self.route]['data'].append(bufs.metadata[self.metadata_key])
+        if "kafka" in metadata:
+            mkafka = metadata["kafka"]
 
-        # write to kafka
-        if bufst0 - self.last_t0 >= self.reduce_time:
-            self.client.write(self.topic, self.kafka_data[self.route], tags=self.tags)
-            self.kafka_data[self.route]['time'] = []
-            self.kafka_data[self.route]['data'] = []
+            # append data to deque
+            if self.metadata_key in mkafka:
+                m = mkafka[self.metadata_key]
+                #if 'time' in m:
+                #self.kafka_data[self.metadata_key]['time'].append(m['time'])
+                #self.kafka_data[self.metadata_key]['data'].append(m['data'])
+                #else:
+                #    self.kafka_data[self.route]['time'].append(bufst0)
+                #    self.kafka_data[self.route]['data'].append(mkafka[self.metadata_key])
 
-            self.last_t0 = bufst0
+                # write to kafka
+                #if bufst0 - self.last_t0 >= self.reduce_time:
+                #self.client.write(self.topic, self.kafka_data[self.metadata_key], tags=self.tags)
+                self.client.write(self.topic, m, tags=self.tags)
+                #self.kafka_data[self.metadata_key]['time'] = []
+                #self.kafka_data[self.metadata_key]['data'] = []
+
+                self.last_t0 = bufst0
         
         if bufs.EOS:
             self.mark_eos(pad)
