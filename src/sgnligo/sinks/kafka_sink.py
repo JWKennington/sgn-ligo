d86850159101970d7094b620026546a70d5e9c8c
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-27 20:12:08 -0400

    kafka_sink.py: update

--- src/sgnligo/sinks/kafka_sink.py
+++ src/sgnligo/sinks/kafka_sink.py
@@ -16,10 +16,8 @@ class KafkaSink(TSSink):
     -----------
     output_kafka_server: str
         The kafka server to write data to
-    topic: str
-        The kafka topic to write data to
-    metadata_key: str
-        The dictionary key of the frame metadata to retrieve the data
+    topics: str
+        The kafka topics to write data to
     tags: str
         The tags to write the kafka data
     verbose: bool
@@ -27,9 +25,9 @@ class KafkaSink(TSSink):
     reduce_time: float
         Will reduce data every reduce_time, in seconds
     """
+
     output_kafka_server: str = None
-    topic: str = None
-    metadata_key: str = None
+    topics: str = None
     prepare_kafka_data: bool = False
     routes: str = None
     tags: list = None
@@ -38,15 +36,14 @@ class KafkaSink(TSSink):
 
     def __post_init__(self):
         assert isinstance(self.output_kafka_server, str)
-        assert isinstance(self.topic, str)
-        assert isinstance(self.metadata_key, str)
+        assert isinstance(self.topics, list)
         super().__post_init__()
 
         self.cnt = {p: 0 for p in self.sink_pads}
         self.last_reduce_time = None
 
         self.client = kafka.Client("kafka://{}".format(self.output_kafka_server))
-        #self.kafka_data = defaultdict(lambda: {'time': [], 'data': []})
+        # self.kafka_data = defaultdict(lambda: {'time': [], 'data': []})
         self.last_t0 = None
 
     def pull(self, pad, bufs):
@@ -55,7 +52,7 @@ class KafkaSink(TSSink):
         graph point and the prints it to prove it all works.
         """
         self.cnt[pad] += 1
-        bufst0 = bufs[0].t0/1_000_000_000
+        bufst0 = bufs[0].t0 / 1_000_000_000
         metadata = bufs.metadata
         if self.last_t0 is None:
             self.last_t0 = bufst0
@@ -64,24 +61,15 @@ class KafkaSink(TSSink):
             mkafka = metadata["kafka"]
 
             # append data to deque
-            if self.metadata_key in mkafka:
-                m = mkafka[self.metadata_key]
-                #if 'time' in m:
-                #self.kafka_data[self.metadata_key]['time'].append(m['time'])
-                #self.kafka_data[self.metadata_key]['data'].append(m['data'])
-                #else:
-                #    self.kafka_data[self.route]['time'].append(bufst0)
-                #    self.kafka_data[self.route]['data'].append(mkafka[self.metadata_key])
+            for topic in self.topics:
+                t = topic.split(".")[-1]
+                if t in mkafka:
+                    self.client.write(topic, mkafka[t], tags=self.tags)
+                else:
+                    raise ValueError(f"{t} not in metadata")
 
-                # write to kafka
-                #if bufst0 - self.last_t0 >= self.reduce_time:
-                #self.client.write(self.topic, self.kafka_data[self.metadata_key], tags=self.tags)
-                self.client.write(self.topic, m, tags=self.tags)
-                #self.kafka_data[self.metadata_key]['time'] = []
-                #self.kafka_data[self.metadata_key]['data'] = []
+            self.last_t0 = bufst0
 
-                self.last_t0 = bufst0
-        
         if bufs.EOS:
             self.mark_eos(pad)
 
@@ -94,5 +82,3 @@ class KafkaSink(TSSink):
         If buffers on any sink pads are End of Stream (EOS), then mark this whole element as EOS
         """
         return any(self.at_eos.values())
-
-
