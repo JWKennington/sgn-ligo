438da65f92b5f525fe9450d23b82eb29b67fcfc7
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-07-31 11:00:40 -0400

    datasource.py: add state vector on bit

--- src/sgnligo/sources/datasource.py
+++ src/sgnligo/sources/datasource.py
@@ -4,13 +4,15 @@ from sgnligo.sources import FrameReader, DevShmSrc
 def datasource(pipeline, options, ifos):
     source_out_links = {ifo: None for ifo in ifos}
     channel_dict = parse_list_to_dict(options.channel_name)
+    state_vector_on_dict = parse_list_to_dict(options.state_vector_on_bits)
     shared_memory_dict = parse_list_to_dict(options.shared_memory_dir)
     for ifo in ifos:
         num_samples = int(options.source_buffer_duration * options.sample_rate)
         if options.data_source == "frames":
+            source_name = "_FrameSource"
             pipeline.insert(
                 FrameReader(
-                    name=ifo + "_Source",
+                    name=ifo + source_name,
                     source_pad_names=(ifo,),
                     rate=options.sample_rate,
                     num_samples=num_samples,
@@ -22,20 +24,24 @@ def datasource(pipeline, options, ifos):
                 ),
             )
         elif options.data_source == "devshm":
+            source_name = "_DevShmSource"
             pipeline.insert(
                 DevShmSrc(
-                    name=ifo + "_Source",
+                    name=ifo + source_name,
                     source_pad_names=(ifo,),
                     rate=16384,
                     num_samples=16384,
                     channel_name=channel_dict[ifo],
                     instrument=ifo,
                     shared_memory_dir=shared_memory_dict[ifo],
+                    state_vector_on_bits=int(state_vector_on_dict[ifo]),
                     wait_time=options.wait_time,
-                    verbose=options.verbose,
+                    #verbose=options.verbose,
+                    verbose=True,
                 ),
             )
         else:
+            source_name = "_FakeSource"
             if options.data_source == "impulse":
                 source_pad_names = (ifo,)
                 signal_type = "impulse"
@@ -50,7 +56,7 @@ def datasource(pipeline, options, ifos):
                 impulse_position = None
             pipeline.insert(
                 FakeSeriesSrc(
-                    name=ifo + "_src",
+                    name=ifo + source_name,
                     source_pad_names=source_pad_names,
                     num_buffers=options.num_buffers,
                     rate=options.sample_rate,
@@ -60,6 +66,6 @@ def datasource(pipeline, options, ifos):
                     verbose=options.verbose,
                 ),
             )
-        source_out_links[ifo] = ifo + "_Source:src:" + ifo
+        source_out_links[ifo] = ifo + source_name + ":src:" + ifo
 
     return source_out_links
