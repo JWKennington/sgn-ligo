650a1c5730abf0bb72e9079abd89eb3bc041ebe2
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-12-13 00:29:57 +0000

    Clean up kafka sink and latency transform

--- src/sgnligo/transforms/latency.py
+++ src/sgnligo/transforms/latency.py
@@ -1,14 +1,18 @@
 from dataclasses import dataclass
 
-from sgnts.base import SeriesBuffer, TSFrame, TSTransform
+from sgn.base import TransformElement
+from sgnts.base import EventBuffer, EventFrame, TSFrame
 
 from sgnligo.base import now
 
 
 @dataclass
-class Latency(TSTransform):
-    """
-    Calculate latency
+class Latency(TransformElement):
+    """Calculate latency and prepare data into the format expected by the KafkaSink
+
+    Args:
+        route:
+            str, the kafka route to send the latency data to
     """
 
     route: str = None
@@ -17,32 +21,37 @@ class Latency(TSTransform):
         super().__post_init__()
         assert len(self.sink_pads) == 1
         assert isinstance(self.route, str)
+        self.frame = None
+
+    def pull(self, pad, frame):
+        self.frame = frame
 
     def new(self, pad):
-        frame = self.preparedframes[self.sink_pads[0]]
-        metadata = frame.metadata
-        time = now()
-        latency = time - frame.buffers[0].t0 / 1_000_000_000
+        """Calculate buffer latency. Latency is defined as the current time subtracted
+        by the buffer start time.
+        """
 
-        outbuf = SeriesBuffer(
-            offset=frame.offset,
-            sample_rate=frame.sample_rate,
-            data=None,
-            shape=frame.shape,
-        )
-        if "kafka" not in metadata:
-            metadata["kafka"] = {}
-        metadata["kafka"][self.route] = {
-            "time": [
-                frame.buffers[0].t0 / 1_000_000_000,
-            ],
-            "data": [
-                latency,
-            ],
+        frame = self.frame
+        time = now().ns()
+        if isinstance(frame, TSFrame):
+            framets = frame.buffers[0].t0
+            framete = frame.buffers[-1].end
+        elif isinstance(frame, EventFrame):
+            framets = next(iter(frame.events.values())).ts
+            framete = next(iter(frame.events.values())).te
+        latency = (time - framets) / 1_000_000_000
+        event_data = {
+            self.route: {
+                "time": [
+                    framets / 1_000_000_000,
+                ],
+                "data": [
+                    latency,
+                ],
+            }
         }
 
-        return TSFrame(
-            buffers=[outbuf],
-            metadata=metadata,
+        return EventFrame(
+            events={"kafka": EventBuffer(ts=framets, te=framete, data=event_data)},
             EOS=frame.EOS,
         )
