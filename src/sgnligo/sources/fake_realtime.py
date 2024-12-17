8fa6a955ecb115ca067e22565eb3a91b1fa65ac5
Yun-Jing Huang <yun-jing.huang@ligo.org>
2024-12-17 20:12:39 +0000

    Gpssrc

---
+++ src/sgnligo/sources/fake_realtime.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from sgnts.sources import RealTimeWhiteNoiseSrc
+
+from sgnligo.base import now as gps_now
+
+
+@dataclass
+class RealTimeWhiteNoiseGPSSrc(RealTimeWhiteNoiseSrc):
+    """A time-series source that generates fake data in fixed-size buffers in real-time
+    and in GPS time
+    """
+
+    def time_now(self):
+        return float(gps_now())
